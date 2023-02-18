f7fc595c789ffdb1871e836444ba3dab25e984ed
albert <xsoularisx@gmail.com>
2023-02-18 15:07:46 +0300

    init: start task brackets, add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrString = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    arrString.push((bracketsConfig[i][0] + bracketsConfig[i][1]));
+  }
+  for (let i = 0; i < arrString.length; i++) {
+    if (str.includes(arrString[i])) {
+      str = str.replace(arrString[i], '');
+      i = -1;
+    }
+  }
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
